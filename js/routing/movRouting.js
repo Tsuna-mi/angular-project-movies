b2710333b8a4be9eeaf9fdd773c6106094953a86
Ana Gamito <Tsuna-mi@users.noreply.github.com>
2016-07-29 15:52:26 +0200

    prueba path

--- js/routing/movRouting.js
+++ js/routing/movRouting.js
@@ -4,23 +4,23 @@ angular.module("movConfig", [])
 
 			$routeProvider
 				.when('/',{
-					templateUrl: '/templates/results.html',
+					templateUrl: 'templates/results.html',
 					controller: 'PopularTmplCtrl'
 				})
 				.when('/popular',{
-					templateUrl: '/templates/results.html',
+					templateUrl: 'templates/results.html',
 					controller: 'PopularTmplCtrl'
 				})
 				.when('/upcoming',{
-					templateUrl: '/templates/results.html',
+					templateUrl: 'templates/results.html',
 					controller: 'UpcomingTmplCtrl'
 				})
 				.when('/nowPlaying',{
-					templateUrl: '/templates/results.html',
+					templateUrl: 'templates/results.html',
 					controller: 'NowPlayingTmplCtrl'
 				})
 				.when('/topRated',{
-					templateUrl: '/templates/results.html',
+					templateUrl: 'templates/results.html',
 					controller: 'TopRatedTmplCtrl'
 				})
 				.otherwise({ redirectTo: '/' }); ;
